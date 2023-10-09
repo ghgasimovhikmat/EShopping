8c73b06dd3e69fed55eb5b3e52b5ebc58fb0cb2f
hikmat <gh.gasimovhikmat@gmail.com>
2023-10-09 14:09:38 +0300

    initi

--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -1,6 +1,61 @@
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 namespace Catalog.API;
 
 public class Startup
 {
-    
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+       
+        // services.AddCors(options =>
+        // {
+        //     options.AddPolicy("CorsPolicy", policy =>
+        //     {
+        //         policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+        //     });
+        // });
+       
+        
+        //DI
+     
+
+        services.AddControllers();
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();  
+         //   app.UseSwagger();
+           // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        // app.UseCors("CorsPolicy");
+        app.UseAuthentication();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+              //  ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        });
+    }
 }
--- Services/Catalog/Catalog.Core/Entities/ProductType.cs
+++ Services/Catalog/Catalog.Core/Entities/ProductType.cs
@@ -1,6 +1,9 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Catalog.Core.Entities;
 
-public class ProductType
+public class ProductType:BaseEntity
 {
-    
+    [BsonElement("Name")]
+    public string Name { get; set; }
 }
